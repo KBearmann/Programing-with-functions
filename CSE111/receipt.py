115c4b03b740b546a23be1c9126c81c733f2f662
KBearmann <keillorbehrmann@gmail.com>
2024-07-06 10:40:27 -0700

    filled out

--- CSE111/receipt.py
+++ CSE111/receipt.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 import os
-
+ 
 def read_dictionary(filename, key_column_index):
     """Read the contents of a CSV file into a compound
     dictionary and return the dictionary.
